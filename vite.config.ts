c149e51a30161ef04c7726d25082736195a476aa
Vishalxsuman <vishalgaurav2692006@gmail.com>
2026-01-07 22:09:44 +0530

    Restore custom domain config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Esporizon-v2/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
